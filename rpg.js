cded535224b5dc9fa7f00d615e5d8282c70aebda
TroyaDevelop <goddys777@icloud.com>
2023-01-17 23:57:38 +0300

    Version 0.02

--- rpg.js
+++ rpg.js
@@ -2,6 +2,7 @@ const player = {
     hp: 100,
     armor: 0,
     power: 1,
+
 }
 
 const enemy = {
@@ -16,12 +17,24 @@ const fight = {
     
     init(){
         document.querySelector(".fight").addEventListener("click", event => {
-            if(event.target.className !== "fight__attack"){
+            if(event.target.className !== "fight__start"){
                 return;
             } else {
-                this.attack(event);
+                this.deleteStartFightButton();
+                this.fightInit();
             };
         });
+    },
+
+    fightInit(){
+        document.querySelector(".fight").addEventListener("click", event =>{
+            if(event.target.className !== "fight__attack"){
+                return;
+            }
+            this.attack(event);
+        });
+        this.createAttackButton();
+        this.createDamageInfo();
     },
 
     run(){
@@ -29,21 +42,82 @@ const fight = {
     },
 
     attack(event){
-        let damage = this.player.power;
+        let damage = Math.floor(Math.random() * 3) + 1;
         if (this.enemy.hp <= 0){
             return;
         }
         this.enemy.hp = this.enemy.hp - damage;
-        this.updateStatus();
-        },
+        if(this.enemy.hp < 0){
+            this.enemy.hp = 0;
+        }
+        this.updateStatus(damage);
+        if(this.enemy.hp === 0){
+            this.endFight();
+        }
+    },
 
-    updateStatus(){
+    updateStatus(damage){
         document.getElementById("player__hp").innerHTML = this.player.hp;
         document.getElementById("player__armor").innerHTML = this.player.armor;
         document.getElementById("player__power").innerHTML = this.player.power;
         document.getElementById("enemy__hp").innerHTML = this.enemy.hp;
-        document.getElementById("enemy__armor").innerHTML = this.enemy.armor;
-        document.getElementById("enemy__power").innerHTML = this.enemy.power;
+        document.getElementById("damageCount").innerHTML = (`${damage} урона`);
+    },
+
+    createDamageInfo(){
+        let fightDiv = document.querySelector(".fight");
+        let damageInfo = document.createElement('p');
+        damageInfo.textContent = "Вы нанесли: ";
+        fightDiv.appendChild(damageInfo);
+        let damageInfo2 = document.createElement('span');
+        damageInfo2.id = "damageCount";
+        damageInfo.appendChild(damageInfo2);
+        return fightDiv;
+    },
+
+    deleteDamageInfo(){
+        let fightDiv = document.querySelector(".fight");
+        fightDiv.innerHTML = "";
+
+        return fightDiv;
+    },
+
+    createStartFightButton(){
+        let fightDiv = document.querySelector(".fight");
+        let startBtn = document.createElement('button');
+        startBtn.textContent = "Начать бой";
+        startBtn.classList.add("fight__start");
+        fightDiv.appendChild(startBtn);
+        return fightDiv;
+    },
+
+    deleteStartFightButton(){
+        let fightDiv = document.querySelector(".fight");
+        let startBtn = document.querySelector(".fight__start");
+        startBtn.remove();
+        return fightDiv;
+    },
+
+    createAttackButton(){
+        let fightDiv = document.querySelector(".fight");
+        let damageInfo = document.createElement('button');
+        damageInfo.textContent = "Атаковать";
+        damageInfo.classList.add("fight__attack");
+        fightDiv.appendChild(damageInfo);
+        return fightDiv;
+    },
+
+    deleteAttackButton(){
+        let fightDiv = document.querySelector(".fight");
+        let attackBtn = document.querySelector(".fight__attack");
+        attackBtn.remove();
+        return fightDiv;
+    },
+
+    endFight(){
+        this.deleteAttackButton();
+        this.deleteDamageInfo();
+        this.createStartFightButton();
     }
 };
 
